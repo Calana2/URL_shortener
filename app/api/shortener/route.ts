840823da82d335c4b7adb04ea7983554bbba6bb5
Calana2 <Protoncala@proton.me>
2024-08-03 18:12:58 -0400

    minorFix

--- app/api/shortener/route.ts
+++ app/api/shortener/route.ts
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import crypto from "crypto"
 import { prisma } from "@/prisma/prisma";
 
-const SITE = "loslocos.com"
 
 export async function POST(req: NextRequest){
  const payload = await req.formData();
+ const SITE = req.url.replace(/^https?:\/\//,'').split('/')[0]
  const url = payload.get('url') as string;
  const ext = Math.floor(Math.random()*(10-2)+2);
  const uuid = crypto.createHash('sha256').update(url).digest('hex').slice(1,ext)
